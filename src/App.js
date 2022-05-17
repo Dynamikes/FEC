cc5f383bf8e7304b90776d9b72e29d79c5020e8b
Michael Zaki <michaelzaki@Michaels-MBP.home>
2022-05-17 16:01:27 -0700

    started contructor on src/app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from "react";
 import { hot } from 'react-hot-loader/root';
 
 class App extends React.Component {
+  constructor(props) {
+
+  }
   render() {
     const { name } = this.props;
     return (
