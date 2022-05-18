b9400fcac94f7ff34f206fb2c1a56d363c0e4f78
Michael Zaki <michaelzaki@Michaels-MBP.home>
2022-05-18 12:14:31 -0700

    Created skeleton components for App.js

---
+++ src/Components/Related_Products/Related_Products.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { hot } from 'react-hot-loader/root';
+
+const Related_Products = (props) => {
+  return (
+    <div>
+      Related Products
+    </div>
+  )
+}
+
+
+export default hot(Related_Products);
