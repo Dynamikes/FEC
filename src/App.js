a1388c04760f929ea5be27df70a9385a4432d772
Tanner Hebert <tannerhebert20xx@gmail.com>
2022-05-18 12:16:47 -0700

    Local changes preceding skeleton pulldown

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-
-import React from "react";
+import React from 'react';
 import { hot } from 'react-hot-loader/root';
 
 class App extends React.Component {
   constructor(props) {
-
+    super(props);
   }
   render() {
     const { name } = this.props;
     return (
       <>
-        <h1>
-          Hello {name}
-        </h1>
+        <h1>Hello {name}</h1>
       </>
     );
   }
