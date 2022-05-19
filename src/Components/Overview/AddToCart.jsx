2fe168e7e9bb1c1a51d76de667143151c5d575f9
Tanner Hebert <tannerhebert20xx@gmail.com>
2022-05-19 16:28:39 -0700

    Begining of flexbox layout

--- src/Components/Overview/AddToCart.jsx
+++ src/Components/Overview/AddToCart.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 // import axios from 'axios';
-
+import styled from 'styled-components';
+const CartDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  background-color: blue;
+  border: 10px;
+  border-color: black;
+`;
+const CartH3 = styled.h3`
+  background-color: blue;
+`;
 const AddToCart = () => {
   return (
-    <div>
-      <h3>AddToCart</h3>
-    </div>
+    <CartDiv>
+      <CartH3>AddToCart</CartH3>
+    </CartDiv>
   );
 };
 
--- src/Components/Overview/Overview.jsx
+++ src/Components/Overview/Overview.jsx
@@ -4,15 +4,42 @@ import ImageView from './ImageView.jsx';
 import StyleSelector from './StyleSelector.jsx';
 import AddToCart from './AddToCart.jsx';
 import ProductInfo from './ProductInfo.jsx';
+import styled from 'styled-components';
+
+const FlexDisplay = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`;
+const StyledProductInfo = styled.div`
+  display: flex;
+  align-self: flex-end;
+  justify-content: center;
+`;
+const ImageStack = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+// const StyledImage = styled.div`
+//   display: flex;
+//   flex-grow: 3;
+// `;
 
 function Overview() {
   return (
     <div>
       <h2>OVERVIEW</h2>
-      <ImageView />
-      <StyleSelector />
-      <AddToCart />
-      <ProductInfo />
+      <ImageStack>
+        <ImageView />
+        <FlexDisplay>
+          <StyleSelector />
+          <AddToCart />
+        </FlexDisplay>
+      </ImageStack>
+
+      <StyledProductInfo>
+        <ProductInfo />
+      </StyledProductInfo>
     </div>
   );
 }
