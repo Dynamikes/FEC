082cbae5389b91c1df9fd1a0f68b276ff15de022
Tanner Hebert <tannerhebert20xx@gmail.com>
2022-05-24 11:42:17 -0700

    Reacquired image pull functionality

--- src/Components/Overview/ImageView.jsx
+++ src/Components/Overview/ImageView.jsx
@@ -50,7 +50,7 @@ function ImageView(props) {
       url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/40344/styles',
       method: 'get',
       headers: {
-        Authorization: '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',
+        Authorization: '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',
       },
     })
       .then((response) => {
