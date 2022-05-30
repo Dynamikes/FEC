35dbbda5c4e5fc4c632ad3fa41bb8250b029e57d
tannerhebert <tannerhebert20xx@gmail.com>
2022-05-30 13:52:52 -0700

    Working on expanded

--- src/Components/Overview/ImageView.jsx
+++ src/Components/Overview/ImageView.jsx
@@ -13,7 +13,8 @@ import {
   HighlightedStyleThumbnail,
   AddOverlay,
   ImagePopUp,
-  AddWrapper
+  AddWrapper,
+  ImageOverlay
 } from '../StyledComponents.jsx';
 import {MAIN_API_KEY} from '../../config.js'
 import {styleIDContext} from './Overview'
@@ -70,7 +71,7 @@ function ImageView(props) {
   const thumbCarousel = [];
   const styleID = useContext(styleIDContext);
   const prodID = useContext(prodIDContext);
-  const [clicked, setClicked] = useState('default')
+  const [clicked, setClicked] = useState(false)
   
   useEffect(()=> {
     if (current === vertCurrent[1]) {
@@ -151,8 +152,9 @@ function ImageView(props) {
 
   return (
     <ImageViewWrapper>
-      <ExpandButton onClick={imageToggle}> Expand </ExpandButton>
-      {vertCurrent[0] === 0 ? (
+      {clicked &&
+      <ImageOverlay> 
+        {vertCurrent[0] === 0 ? (
         ''
       ) : (
         <StyledUpArrow className='left-arrow'  onClick={() => changeVertCurrent('up')} />
@@ -163,26 +165,7 @@ function ImageView(props) {
         <StyledLeftArrow className='left-arrow' onClick={prevImage} />
       )}
       
-      {clicked === 'expanded' ? 
-      <AddOverlay onClick={() => setCurrentPicture('')}>
-          <AddWrapper>
-            {current === 0 ? (
-          ''
-        ) : (
-          <ZoomedLeftArrow className='left-arrow' onClick={prevImage} />
-        )}
-            <ImagePopUp src={currentPicture} />
-            {current === carLength - 1 ? (
-        ''
-      ) : (
-        <ZoomedRightArrow className='right-arrow' onClick={nextImage} />
-      )}
-          </AddWrapper>
-        </AddOverlay> 
-        :
-        null
-        
-      }
+      
       {loaded
         ? CarouselData.map((picture, index) => {
           
@@ -191,7 +174,7 @@ function ImageView(props) {
                 className={index === current ? 'slide active' : 'slide'}
                 key={index}>
                 {index === current && (
-                  <MainImage key={index} src={picture} alt='style image' onClick={() => {setCurrentPicture(picture), setClicked('expanded')}} />
+                  <MainImage key={index} src={picture} alt='style image' onClick={() => {setCurrentPicture(picture), setClicked(true)}} />
                 )}
               </ImageWrapper>
             )
@@ -227,7 +210,69 @@ function ImageView(props) {
         <StyledRightArrow className='right-arrow' onClick={nextImage} />
       )}
     
-      {vertCurrent[0] === carLength - 1 ? (
+      {vertCurrent[1] === carLength - 1 ? (
+        ''
+      ) : (
+        <StyledDownArrow className='right-arrow'  onClick={() => changeVertCurrent('down')} />
+      )}
+      </ImageOverlay> }
+      {vertCurrent[0] === 0 ? (
+        ''
+      ) : (
+        <StyledUpArrow className='left-arrow'  onClick={() => changeVertCurrent('up')} />
+      )}
+      {current === 0 ? (
+        ''
+      ) : (
+        <StyledLeftArrow className='left-arrow' onClick={prevImage} />
+      )}
+      
+      
+      {loaded
+        ? CarouselData.map((picture, index) => {
+          
+            return (
+              <ImageWrapper
+                className={index === current ? 'slide active' : 'slide'}
+                key={index}>
+                {index === current && (
+                  <MainImage key={index} src={picture} alt='style image' onClick={() => {setCurrentPicture(picture), setClicked(true)}} />
+                )}
+              </ImageWrapper>
+            )
+          })
+        : ''}
+<Thumbnails>
+        {loaded
+          ? CarouselData.slice(vertCurrent[0], vertCurrent[1]).map((thumbnail, index) => {
+              
+            if (thumbnail === CarouselData[current]) {
+              return (
+                <HighlightedStyleThumbnail
+                  key={index}
+                  src={thumbnail}
+                  alt={'style thumbnail'}
+                  
+                />
+              )
+            } else {
+              return (<ThumbnailImage
+                  key={index}
+                  src={thumbnail}
+                  alt={'style thumbnail'}
+                  onClick={() => changeCurrent(index)}
+                />)
+            }
+            })
+          : ''}
+      </Thumbnails>
+      {current === carLength - 1 ? (
+        ''
+      ) : (
+        <StyledRightArrow className='right-arrow' onClick={nextImage} />
+      )}
+    
+      {vertCurrent[1] === carLength - 1 ? (
         ''
       ) : (
         <StyledDownArrow className='right-arrow'  onClick={() => changeVertCurrent('down')} />
@@ -236,3 +281,23 @@ function ImageView(props) {
   );
 }
 export default hot(ImageView);
+{/* {clicked === 'expanded' ? 
+      <AddOverlay onClick={() => setCurrentPicture('')}>
+          <AddWrapper>
+            {current === 0 ? (
+          ''
+        ) : (
+          <ZoomedLeftArrow className='left-arrow' onClick={prevImage} />
+        )}
+            <ImagePopUp src={currentPicture} />
+            {current === carLength - 1 ? (
+        ''
+      ) : (
+        <ZoomedRightArrow className='right-arrow' onClick={nextImage} />
+      )}
+          </AddWrapper>
+        </AddOverlay> 
+        :
+        null
+        
+      } */}
